fa2c9e84c499c80d041c4bb9b2ec0d42a34b6826
paingha <apeelit@gmail.com>
2020-11-12 23:11:50 -0600

    added mock to register user test

--- api/models/user_test.go
+++ api/models/user_test.go
@@ -7,16 +7,25 @@ package models_test
 import(
 	"testing"
 	"log"
+	"encoding/base64"
 	"bitbucket.com/irb/api/config"
+	"bitbucket.com/irb/api/utils"
 	"bitbucket.com/irb/api/models"
+	"github.com/brianvoe/gofakeit/v5"
 )
 
 func TestCreateUser(t *testing.T){
 	if err := config.UseDBTestContext(); err != nil{
 		log.Fatal(err)
 	}
-	var user models.User
-	//Add mock user data here
+	gofakeit.Seed(0)
+	user := models.User{
+		FirstName: gofakeit.FirstName(),
+		LastName: gofakeit.LastName(),
+		Email: gofakeit.Email(),
+		PhoneNumber: gofakeit.Phone(),
+		Password: gofakeit.Password(true, true, true, true, true, 6),
+	}
 	status, err := models.CreateUser(&user)
 	if err != nil {
 		log.Print(err)
@@ -70,5 +79,105 @@ func TestGetUsers(t *testing.T){
 	} else {
 		log.Println(users)
 		log.Println(count)
+	}
+}
+
+func TestSendVerifyPhoneUser(t *testing.T){
+	if err := config.UseDBTestContext(); err != nil{
+		log.Fatal(err)
+	}
+	code := utils.GenerateRandomInt(5)
+	medium := "whatsapp"
+	var user models.User
+	var id int;
+	err := models.SendVerifyPhoneUser(&user, id, code, medium)
+	if err != nil {
+		log.Print(err)
+	} else {
+		log.Println("Code sent successfully")
+	}
+}
+
+func TestForgotUser(t *testing.T){
+	var user models.User
+	if err := config.UseDBTestContext(); err != nil{
+		log.Fatal(err)
+	}
+	resp, err := models.ForgotUser(&user)
+	if err != nil{
+		log.Print(err)
+	}else{
+		if !resp && err == nil {
+			log.Println("Could not send the reset email")
+		} else {
+			log.Println("Password Reset Email sent successfully")
+		}
+	}
+}
+
+func TestDeleteUser(t *testing.T){
+	var id int = 0;
+	if err := config.UseDBTestContext(); err != nil{
+		log.Fatal(err)
+	}
+	err := models.DeleteUser(id)
+	if err != nil{
+		log.Print(err)
+	}else{
+		log.Println("User Deleted successfully")
+	}
+}
+
+func TestUpdateUser(t *testing.T){
+	var user models.User
+	var id int = 0;
+	if err := config.UseDBTestContext(); err != nil{
+		log.Fatal(err)
+	}
+	err := models.UpdateUser(&user, id)
+	if err != nil{
+		log.Print(err)
+	}else{
+		log.Println("Password Reset Email sent successfully")
+	}
+}
+
+func TestVerifyEmailUser(t *testing.T){
+	var token string = ""
+	var user models.User
+	if err := config.UseDBTestContext(); err != nil{
+		log.Fatal(err)
+	}
+	if tokenDecoded, err := base64.StdEncoding.DecodeString(token); err != nil {
+		log.Println("Email verify token decode error")
+	} else {
+		log.Println(string(tokenDecoded))
+		token = string(tokenDecoded)
+	}
+	err := models.VerifyEmailUser(&user, token)
+	if err != nil{
+		log.Print(err)
+	}else{
+		log.Println("Password Reset Email sent successfully")
+	}
+}
+
+func TestVerifyPhoneCodeUser(t *testing.T){
+	var user models.User
+	var token string = ""
+	var id int = 0;
+	if err := config.UseDBTestContext(); err != nil{
+		log.Fatal(err)
+	}
+	state, errs := models.VerifyPhoneUser(&user, id, token)
+	if errs != nil {
+		log.Println("An error occured while verifying user's phone number")
+		log.Println(errs)
+	} else {
+		if state == true && errs == nil {
+			log.Println("Phone verifed successfully")
+		} else {
+			log.Println("Token expired or invalid")
+		}
 	}
 }
--- go.mod
+++ go.mod
@@ -6,9 +6,11 @@ require (
 	github.com/Netflix/go-env v0.0.0-20200803161858-92715955ff70
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
 	github.com/aws/aws-sdk-go v1.34.0
+	github.com/brianvoe/gofakeit/v5 v5.10.1
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
 	github.com/gofrs/uuid v3.3.0+incompatible
+	github.com/golang/mock v1.4.4 // indirect
 	github.com/gorilla/websocket v1.4.2
 	github.com/jinzhu/gorm v1.9.15
 	github.com/sendgrid/rest v2.6.0+incompatible // indirect
